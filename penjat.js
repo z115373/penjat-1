6319095d9a7d557d76471df1d9f2dbee1960a1d7
z115373 <123448646+z115373@users.noreply.github.com>
2023-02-14 09:50:48 +0100

    Update penjat.js

--- penjat.js
+++ penjat.js
@@ -1,20 +1,55 @@
- 
- var vidas= 7;
+  var vidas= 7;
   var palabro="";
   var letra="";
   function Verificar(){
   var lletra= document.getElementById("lletra").value;
-  if((lletra >= "m")) {
+  
+  switch (lletra) {
+    case 'à':
+    case 'á':
+    case 'ä':
+    lletra='a';
+         break;
+    case 'è':
+    case 'é':
+    case 'ë':
+    lletra='e';
+         break;
+    case 'ì':
+    case 'í':
+    case 'ï':
+    lletra='i';
+         break;
+    case 'ò':
+    case 'ó':
+    case 'ö':
+    lletra='o';
+         break;
+    case 'ú':
+    case 'ù':
+    case 'ü':
+    lletra='u';
+         break;
+    case 'ñ':
+    lletra='n';
+         break;
+    case'ç':
+    lletra='c';
+         break;
+}
+  if((lletra >= "m")){
       vidas= vidas-1;
       document.getElementById("vidas").innerHTML=vidas;
       letra=lletra+letra+""
       document.getElementById("letras").innerHTML=letra;
+      document.body.style.backgroundImage="url('img/Jungle.png')"
       document.getElementById('Fallo').play();
       document.getElementById('tictac').play();
 } else{
       palabro=lletra+palabro+""
       document.getElementById("palabro").innerHTML=palabro;
       document.getElementById('G2').play();
+      document.body.style.backgroundImage="url('img/Desert.png')"
 }
   if ((vidas==0)){
       window.alert("Has perdut");
@@ -22,41 +57,37 @@
       document.getElementById("boton").disabled=true;
       document.getElementById("A0").hidden=false;
       document.getElementById("A1").hidden=true;
-      document.body.style.backgroundImage="url('img/Jungle.png')"
+      document.body.style.backgroundImage="url('img/Castle2.png')"
       document.getElementById('campanadax3').play();
       document.getElementById('G1').play();
       lletra="";
   }   
   if ((vidas==6)){
-      window.alert("malament");
       document.getElementById("A6").hidden=false;
   }
   if ((vidas==5)){
-      window.alert("malament");
       document.getElementById("A5").hidden=false;
       document.getElementById("A6").hidden=true;
   }
   if ((vidas==4)){
-      window.alert("malament");
       document.getElementById("A4").hidden=false;
       document.getElementById("A5").hidden=true;    
   }
   if ((vidas==3)){
-      window.alert("malament");
       document.getElementById("A3").hidden=false;
       document.getElementById("A4").hidden=true;    
   }
   if ((vidas==2)){
-      window.alert("malament");
       document.getElementById("A2").hidden=false;
       document.getElementById("A3").hidden=true;  
   }
   if ((vidas==1)){
-      window.alert("malament");
       document.getElementById("A1").hidden=false;
       document.getElementById("A2").hidden=true;  
  }
- }   
+ }
+
+ 
 
 
     function Esconder(){
@@ -69,4 +100,5 @@
     document.getElementById("A6").hidden=true;
     document.getElementById("fondo1").hidden=true;
     document.getElementById("fondo2").hidden=true;
+    document.getElementById("fondo3").hidden=true;
     }
